a0915e6e9ad134cd2ebf27dc867f67ba68320877
chaka.hu <chaka.hu@ximalaya.com>
2020-12-07 13:38:11 +0800

    tcc 框架调试

--- tcc-transaction-tutorial-sample/tcc-transaction-dubbo-sample/tcc-transaction-dubbo-capital/src/main/java/org/mengyun/tcctransaction/sample/dubbo/capital/service/CapitalAccountServiceImpl.java
+++ tcc-transaction-tutorial-sample/tcc-transaction-dubbo-sample/tcc-transaction-dubbo-capital/src/main/java/org/mengyun/tcctransaction/sample/dubbo/capital/service/CapitalAccountServiceImpl.java
@@ -1,10 +1,12 @@
 package org.mengyun.tcctransaction.sample.dubbo.capital.service;
 
+import org.mengyun.tcctransaction.sample.capital.domain.entity.CapitalAccount;
 import org.mengyun.tcctransaction.sample.capital.domain.repository.CapitalAccountRepository;
 import org.mengyun.tcctransaction.sample.dubbo.capital.api.CapitalAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
 
 /**
@@ -20,5 +22,11 @@ public class CapitalAccountServiceImpl implements CapitalAccountService{
     @Override
     public BigDecimal getCapitalAccountByUserId(long userId) {
         return capitalAccountRepository.findByUserId(userId).getBalanceAmount();
+    }
+
+    @PostConstruct
+    public void afterProperties(){
+        CapitalAccount byUserId = capitalAccountRepository.findByUserId(1000L);
+        System.out.println(byUserId);
     }
 }
